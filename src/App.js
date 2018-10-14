7c9b3631a5dedd32679ab347f7dea0eb084a645a
Marcin Rudzki <mar.rudzki@gmail.com>
2018-10-14 22:14:27 +0200

    Add isLoading and photos to state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = corsAnywhere + 'http://shibe.online/api';
+
 class App extends Component {
     state = {
         count: 1,
+        isLoading: false,
+        photos: [],
         type: "shibes"
     };
 
@@ -14,12 +19,25 @@ class App extends Component {
         this.setState({ type: event.target.value })
     };
 
+    onSubmitForm = event => {
+        event.preventDefault();
+        this.setState({
+            isLoading: true
+        })
+        fetch(`${apiUrl}/${this.state.type}?count=${this.state.count}`)
+            .then(response => response.json())
+            .then(photos => this.setState({
+                isLoading: false,
+                photos
+            }));
+    }
+
     render() {
         return (
             <div className="container">
                 <h1>React Native Has Power</h1>
                 <h2>zadanie weryfikujące</h2>
-                <form className="form-inline">
+                <form className="form-inline" onSubmit={this.onSubmitForm}>
                     <label htmlFor="count">Ilość</label>
                     <input id="count" className="form-control" type="number" name="count" min="1" max="10"
                            value={this.state.count} onChange={this.onCountChange}/>
@@ -30,7 +48,9 @@ class App extends Component {
                         <option value="birds">ptaki</option>
                         <option value="random">losowe</option>
                     </select>
-                    <button className="btn btn-primary" type="submit">Szukaj</button>
+                    <button className="btn btn-primary" type="submit" disabled={this.state.isLoading}>
+                        {this.state.isLoading ? 'Ładowanie danych' : 'Szukaj'}
+                    </button>
                 </form>
             </div>
         );
