872d4510d7d022bad32878a4e1bcf87c2d0e5dc0
Marcin Rudzki <mar.rudzki@gmail.com>
2018-10-16 22:08:44 +0200

    Extract SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React, { Component } from 'react';
-
-const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = corsAnywhere + 'http://shibe.online/api';
+import SearchForm from './SearchForm';
 
 class App extends Component {
-    state = {
-        count: 1,
-        isLoading: false,
-        photos: [],
-        type: "shibes"
-    };
-
-    onCountChange = event => {
-        this.setState({ count: parseInt(event.target.value) })
-    };
-
-    ontTypeChange = event => {
-        this.setState({ type: event.target.value })
-    };
-
-    getRandomType() {
-        const randomIndex = Math.floor(Math.random() * 3);
-        return ["shibes", "cats", "birds"][randomIndex];
+    constructor() {
+        super();
+        this.state = {
+            photos: []
+        }
+        this.updatePhotos = this.updatePhotos.bind(this);
     }
 
-    onSubmitForm = event => {
-        event.preventDefault();
-        this.setState({
-            isLoading: true
-        });
-        const endpoint = this.state.type === 'random' ? this.getRandomType() : this.state.type;
-        fetch(`${apiUrl}/${endpoint}?count=${this.state.count}`)
-            .then(response => response.json())
-            .then(photos => this.setState({
-                isLoading: false,
-                photos
-            }));
+    updatePhotos(photos) {
+        this.setState({ photos })
     }
 
     render() {
@@ -43,21 +19,7 @@ class App extends Component {
             <div className="container">
                 <h1>React Native Has Power</h1>
                 <h2>zadanie weryfikujące</h2>
-                <form className="form-inline" onSubmit={this.onSubmitForm}>
-                    <label htmlFor="count">Ilość</label>
-                    <input id="count" className="form-control" type="number" name="count" min="1" max="10"
-                           value={this.state.count} onChange={this.onCountChange}/>
-                    <label htmlFor="type">Typ</label>
-                    <select id="type" name="type" className="form-control" onChange={this.ontTypeChange}>
-                        <option value="shibes">psy</option>
-                        <option value="cats">koty</option>
-                        <option value="birds">ptaki</option>
-                        <option value="random">losowe</option>
-                    </select>
-                    <button className="btn btn-primary" type="submit" disabled={this.state.isLoading}>
-                        {this.state.isLoading ? 'Ładowanie danych' : 'Szukaj'}
-                    </button>
-                </form>
+                <SearchForm onUpdatePhotos={this.updatePhotos}/>
                 <div className="search-results">
                     {this.state.photos.map((photo, index) => (
                         <img src={photo} alt="" className="img-thumbnail" key={index}/>
